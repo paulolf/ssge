2cbf55169fe693e0beb3464bc4df6151b351a874
Paulo Ferreira <paulolf@gmail.com>
2017-02-07 17:41:56 -0200

    open and save zip files

---
+++ StellarisSaveGameEditor/Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace StellarisSaveGameEditor
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStripMain = new System.Windows.Forms.StatusStrip();
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStripMain
+            // 
+            this.statusStripMain.Location = new System.Drawing.Point(0, 239);
+            this.statusStripMain.Name = "statusStripMain";
+            this.statusStripMain.Size = new System.Drawing.Size(284, 22);
+            this.statusStripMain.TabIndex = 0;
+            this.statusStripMain.Text = "statusMain";
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(284, 24);
+            this.menuStripMain.TabIndex = 1;
+            this.menuStripMain.Text = "menuStripMain";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.statusStripMain);
+            this.Controls.Add(this.menuStripMain);
+            this.MainMenuStrip = this.menuStripMain;
+            this.Name = "Main";
+            this.Text = "Stellaris Savegame Editor";
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStripMain;
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ StellarisSaveGameEditor/Main.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using StellarisSaveGameEditor.Properties;
+using System.IO.Compression;
+
+namespace StellarisSaveGameEditor
+{
+    public partial class Main : Form
+    {
+        public string GamestateString { get; set; }
+        public string MetaString { get; set; }
+        public string ZipFileName { get; set; }
+        public string InitialDirectory { get; set; }
+
+        public Main()
+        {
+            InitializeComponent();
+
+            saveToolStripMenuItem.Enabled = false;
+            InitialDirectory = @"C:\Users\paulo\OneDrive\Documentos\Paradox Interactive\Stellaris\save games\gatineofoundation_-154020303";
+            //InitialDirectory = "c:\\"
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                InitialDirectory = InitialDirectory,
+                Filter = "sav files (*.sav)|*.sav",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            try
+            {
+                var zipFile = openFileDialog.OpenFile();
+
+                ZipFileName = ((FileStream)zipFile).Name;
+
+                using (zipFile)
+                {
+                    using (var zip = new ZipArchive(zipFile, ZipArchiveMode.Read))
+                    {
+                        foreach (var entry in zip.Entries)
+                        {
+                            using (var stream = entry.Open())
+                            {
+                                var reader = new StreamReader(stream);
+
+                                if (entry.FullName == "meta")
+                                    MetaString = reader.ReadToEnd();
+
+                                if (entry.FullName == "gamestate")
+                                    GamestateString = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+
+                saveToolStripMenuItem.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Resources.Main_openToolStripMenuItem_Click_Error__Could_not_read_file_from_disk__Original_error__ + ex.Message);
+            }
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+                {
+                    var gamestateFile = archive.CreateEntry("gamestate");
+
+                    using (var entryStream = gamestateFile.Open())
+                    using (var streamWriter = new StreamWriter(entryStream))
+                    {
+                        streamWriter.Write(GamestateString);
+                    }
+
+                    var metaFile = archive.CreateEntry("meta");
+
+                    using (var entryStream = metaFile.Open())
+                    using (var streamWriter = new StreamWriter(entryStream))
+                    {
+                        streamWriter.Write(MetaString);
+                    }
+                }
+
+                var saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "sav files (*.sav)|*.sav",
+                    Title = "Save"
+                };
+                saveFileDialog.ShowDialog();
+
+                if (saveFileDialog.FileName == "") return;
+
+                using (var fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    memoryStream.CopyTo(fileStream);
+                }
+            }
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
